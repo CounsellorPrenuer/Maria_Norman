85fd9e4ec240853ace992901b7c974edcb0b861d
Mihika Sarwate <mihika@example.com>
2026-09-12 10:18:23 +0530

    Convert to pure static HTML fetching Sanity via CSR

--- sanity/schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import siteSettings from './siteSettings';
-import homePage from './home';
-import founder from './founder';
-import service from './service';
-import mentoriaPackage from './mentoriaPackage';
-
-export const schema = {
-  types: [siteSettings, homePage, founder, service, mentoriaPackage],
-};
